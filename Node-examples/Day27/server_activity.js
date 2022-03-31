3117bb6501b5b5fa04a631796f92edeb604ed38c
Jayesh_Saindane <www.jayeshsaindane.js@gmail.com>
2022-03-31 10:19:47 +0530

    Day 27

--- Node-examples/Day27/server_activity.js
+++ Node-examples/Day27/server_activity.js
@@ -1,3 +1,4 @@
+
 let express=require('express');
 let app=express();
 let http=require('http').Server(app);
@@ -12,7 +13,17 @@ io.on('connection',(socket)=>{
     //console.log(a)
     console.log('Some client connected.');
     socket.on('chat',(msg)=>{
-        io.emit('chat',`${useragent[0]} ${msg}`);
+        var UAparser = require('ua-parser').parse;
+        var useragent = UAparser(socket.request.headers['user-agent']).ua.toString().split(' ');
+        
+        console.log(useragent[0]);
+       // console.log(msg);
+       if(useragent[0]=='Firefox'){
+           socket.broadcast.emit('chat', `${useragent[0]} ${msg}`);
+
+       }else if (useragent[0]=='Chrome'){
+            socket.broadcast.emit('chat', `${useragent[0]} ${msg}`);
+       }
     })
 });
 http.listen(3002,()=>console.log('Server is running in 3002 '))
